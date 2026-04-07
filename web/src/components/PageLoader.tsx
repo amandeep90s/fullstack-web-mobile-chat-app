9afd4ea44f64758d1cc30c30d2d84e20300b75a2
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-04-07 13:13:27 +0530

    feat(web): enhance PageLoader component with loading animation

--- web/src/components/PageLoader.tsx
+++ web/src/components/PageLoader.tsx
@@ -1,3 +1,9 @@
+import { LoaderIcon } from "lucide-react";
+
 export default function PageLoader() {
-	return <div>PageLoader</div>;
+	return (
+		<div className="flex items-center justify-center h-screen bg-black">
+			<LoaderIcon className="size-6 animate-spin text-orange-500" />
+		</div>
+	);
 }
